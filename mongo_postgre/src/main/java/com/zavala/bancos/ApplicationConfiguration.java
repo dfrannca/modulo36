59a4837be06e2df26d64cf41f3b316aed9c83e11
Marcos D'França <marcosdfrancass@gmail.com>
2025-02-15 09:02:14 -0300

    firstcommit

---
+++ mongo_postgre/src/main/java/com/zavala/bancos/ApplicationConfiguration.java
@@ -0,0 +1,11 @@
+package com.zavala.bancos;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Ramiro
+ *
+ */
+@SpringBootApplication
+public class ApplicationConfiguration {}
